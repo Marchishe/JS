d9df537bb4bd8486c123d1078d62fa5b06705c32
Marko <markovolchenko@gmail.com>
2023-05-31 17:14:17 +0300

    start

---
+++ data-types.js
@@ -0,0 +1,23 @@
+//const vs let
+const name = 'Marko'
+let age = 24
+
+//data types
+const name = 'Oleg'
+const age = 27
+const married = true
+const cars = ['lanos', 'bmw' ]
+const person = {
+    name:'Oleg',
+    age: 27,
+    married: true,
+    cars:['lanos', 'bmw']
+}
+const person2 = {
+    name,
+    age,
+    married,
+    cars
+}
+const rndm = undefined
+const rndm2 = null
---
+++ main.js
@@ -0,0 +1,10 @@
+//functions
+function getName(){
+    return 'Marko'
+}
+getName()
+const getSurname = () => 'Volchenko'
+getSurname()
+const getPrice = () =>{
+
+}
